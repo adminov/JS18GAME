a071c7dccd3198b5531d71c270a57271947365ad
Babur <batishnazik@gmail.com>
2021-04-17 01:32:42 +0600

    update 6 lesson

--- js.js
+++ js.js
@@ -7,29 +7,26 @@ const user = () => {
     let num = 55;
     const numFromUser = () => {
         let numUser;
-        numUser = +prompt('Угадай число от 1 до 100');
+        numUser = prompt('Угадай число от 1 до 100');
         console.log(typeof  numUser);
-        if (numUser === num){
-            alert('Поздравляю, Вы угадали!!!');
+        if (numUser === null){
+            alert('Игра окончена');
+            return;
+        } else if (numUser < num){
+            alert('Загаданное число больше');
+            numFromUser();
         }
-        else {
-            if (numUser < num){
-                alert('Загаданное число меньше');
-                numFromUser();
-            }
-            if (numUser > num) {
-                alert('Загаданное число больше');
-                numFromUser();
-            }
-            if (!isNumber(numUser)){
-                alert('Введи число!');
-                numFromUser();
-            } else {
-                alert('Игра окончена');
-            }
+        else if (numUser > num){
+            alert('Загаданное число меньше');
+            numFromUser();
+        }
+        else if  (!isNumber(numUser)){
+            alert('Введи число!');
+            numFromUser();
+        } else {
+            alert('Поздравляю, Вы угадали!!!');
         }
     };
     return numFromUser();
 };
-
 user();
